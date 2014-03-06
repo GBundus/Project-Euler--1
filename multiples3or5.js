929ba017fee3657b2007d39354c5f09170d60ac6
Gregory Bundus <gregory.bundus@gmail.com>
2014-03-06 14:57:04 -0500

    Uploading Solution

---
+++ multiples3or5.js
@@ -0,0 +1,11 @@
+function sumMultiplesOf3or5(x){
+//takes a integer x and returns the sum of multiples of 3 or five less than it
+	output=0;
+	for(i=0;i<=x;i++){
+		if (i%3==0 || i%5==0){
+			output+=i;
+		}
+	}
+	document.getElementById('outbox').innerHTML=output;
+
+}
